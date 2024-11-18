01530b030c1f6a132b0242b9c6fe4cbb6f807b0a
Ricardflores <ricardo.flores@devitm.com>
2024-11-18 11:53:49 -0600

    seccion 6 completa errores

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	</div> <!--row -->
 	</div><!-- content container--> 
 
-			<footer id="colophon" class="site-footer">
+			<footer id="colophon" class="site-footer row">
 				<div class="container">
 				<div class="row">
 						<div class="col-md-4">
@@ -30,32 +30,32 @@
 							?>
 						</div>
 
-						<div class="col-md-4">
-						<div id="footer 3" class="widget-area">
-							<?php 
-								dynamic_sidebar( 'footer 3' ); 
-							?>
+					</div>    <!-----row-------->
+
+					<div class="site-info row ">
+						<div class="col-md-12 text-center">
+							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'primerapagina' ) ); ?>">
+						<?php
+								/* translators: %s: CMS name, i.e. WordPress. */
+									printf( esc_html__( 'Proudly powered by %s', 'primerapagina' ), 'WordPress' );
+						?>
+							</a>
+							<span class="sep"> | </span>
+						<?php
+							/* translators: 1: Theme name, 2: Theme author. */
+							printf( esc_html__( 'Theme: %1$s by %2$s.', 'primerapagina' ), 'primerapagina', '<a href="http://underscores.me/">Underscores.me</a>' );
+						?>
+
 						</div>
 
-					</div>    <!-----row-------->
+						
+		</div><!-- .site-info -->
+
 				</div>        <!-----fin container-------->
 
 
 				
-				<div class="site-info">
-					<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'primerapagina' ) ); ?>">
-						<?php
-						/* translators: %s: CMS name, i.e. WordPress. */
-						printf( esc_html__( 'Proudly powered by %s', 'primerapagina' ), 'WordPress' );
-						?>
-					</a>
-					<span class="sep"> | </span>
-						<?php
-						/* translators: 1: Theme name, 2: Theme author. */
-						printf( esc_html__( 'Theme: %1$s by %2$s.', 'primerapagina' ), 'primerapagina', '<a href="http://underscores.me/">Underscores.me</a>' );
-						?>
-						
-		</div><!-- .site-info -->
+
 	</footer><!-- #colophon -->
 </div><!-- #page --> 
 
--- functions.php
+++ functions.php
@@ -143,24 +143,11 @@ function primerapagina_widgets_init() {
 	)
 	);
 
-
-	register_sidebar( 
-		array(
-		'name'          => esc_html__( 'Footer -Zone 2', 'primerapagina' ),
-		'id'            => 'Footer 2',
-		'description'   => esc_html__( 'Add widgets here Footer Zone 2.', 'primerapagina' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-)
-);
-
 register_sidebar( 
 	array(
-	'name'          => esc_html__( 'Footer -Zone 3', 'primerapagina' ),
+	'name'          => esc_html__( 'Footer -Zone 2', 'primerapagina' ),
 	'id'            => 'Footer 2',
-	'description'   => esc_html__( 'Add widgets here Footer Zone 3.', 'primerapagina' ),
+	'description'   => esc_html__( 'Add widgets here Footer Zone 2.', 'primerapagina' ),
 	'before_widget' => '<section id="%1$s" class="widget %2$s">',
 	'after_widget'  => '</section>',
 	'before_title'  => '<h2 class="widget-title">',
